879d2a4881008b59eb3be1f38112bae2ec6c5a76
Yevhenii Bilyk <yevhenii.bilyk@flexum.io>
2022-07-18 16:35:48 +0300

    Rewrite tests

--- src/options/mergeOptions.test.ts
+++ src/options/mergeOptions.test.ts
@@ -1,66 +1,74 @@
 import { mergeWithDefaultOptions, defaultExportOptions, DocxExportOptions, PageFormat, PageOrientation } from '.';
 
-test('mergeWithDefaultOptions should return default options if called without arguments', () => {
-  const options = mergeWithDefaultOptions();
+describe('mergeWithDefaultOptions', () => {
+  describe('called without argument', () => {
+    test('should return default options if no option provided', () => {
+      const expectedResult = defaultExportOptions;
 
-  expect(options).toMatchObject<DocxExportOptions>(defaultExportOptions);
-});
+      const result = mergeWithDefaultOptions();
 
-describe('mergeWithDefaultOptions should return right object if called with arguments', () => {
-  test('should merge page options', () => {
-    const testOptions = {
-      page: {
-        orientation: PageOrientation.Landscape,
-        size: PageFormat.A5,
-        margins: { top: 10, right: 10 },
-      },
-    };
-
-    const options = mergeWithDefaultOptions(testOptions);
-
-    const expectedOptions = {
-      ...defaultExportOptions,
-      page: {
-        ...defaultExportOptions.page,
-        ...testOptions.page,
-        margins: { ...defaultExportOptions.page.margins, ...testOptions.page.margins },
-      },
-    };
-
-    expect(options).toMatchObject<DocxExportOptions>(expectedOptions);
+      expect(result).toMatchObject<DocxExportOptions>(expectedResult);
+    });
   });
 
-  test('should merge font options', () => {
-    const testOptions = {
-      font: {
-        baseSize: 40,
-        baseFontFamily: 'Times New Roman',
-        headersSizes: {
-          h1: 45,
-          h2: 43,
+  describe('called with user input argument', () => {
+    test('should merge page options', () => {
+      const testOptions = {
+        page: {
+          orientation: PageOrientation.Landscape,
+          size: PageFormat.A5,
+          margins: { top: 10, right: 10 },
         },
-      },
-    };
+      };
+      const expectedOptions = {
+        ...defaultExportOptions,
+        page: {
+          ...defaultExportOptions.page,
+          ...testOptions.page,
+          margins: { ...defaultExportOptions.page.margins, ...testOptions.page.margins },
+        },
+      };
 
-    const options = mergeWithDefaultOptions(testOptions);
+      const options = mergeWithDefaultOptions(testOptions);
 
-    const expectedOptions = {
-      ...defaultExportOptions,
-      font: {
-        ...defaultExportOptions.font,
-        ...testOptions.font,
-        headersSizes: { ...defaultExportOptions.font.headersSizes, ...testOptions.font.headersSizes },
-      },
-    };
+      expect(options).toMatchObject<DocxExportOptions>(expectedOptions);
+    });
 
-    expect(options).toMatchObject<DocxExportOptions>(expectedOptions);
+    test('should merge font options', () => {
+      const testOptions = {
+        font: {
+          baseSize: 40,
+          baseFontFamily: 'Times New Roman',
+          headersSizes: {
+            h1: 45,
+            h2: 43,
+          },
+        },
+      };
+      const expectedOptions = {
+        ...defaultExportOptions,
+        font: {
+          ...defaultExportOptions.font,
+          ...testOptions.font,
+          headersSizes: { ...defaultExportOptions.font.headersSizes, ...testOptions.font.headersSizes },
+        },
+      };
+
+      const options = mergeWithDefaultOptions(testOptions);
+
+      expect(options).toMatchObject<DocxExportOptions>(expectedOptions);
+    });
   });
-});
 
-test('mergeWithDefaultOptions should throw error when called with wrong arguments', () => {
-  const testOptions = {
-    foo: { bar: 'baz' },
-  };
+  describe('called with invalid arguments', () => {
+    test('should throw exception', () => {
+      const testOptions = {
+        foo: { bar: 'baz' },
+      };
 
-  expect(() => mergeWithDefaultOptions(testOptions as unknown as DocxExportOptions)).toThrowError();
+      const fn = () => mergeWithDefaultOptions(testOptions as unknown as DocxExportOptions);
+
+      expect(fn).toThrowError();
+    });
+  });
 });
