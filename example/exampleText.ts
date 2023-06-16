c7ac1e62c7e98dfec5c6f2b5ec5528b2c74ccc60
zhebil <jbilyk1996@gmail.com>
2023-06-16 18:53:24 +0300

    fix: Change example text

--- example/exampleText.ts
+++ example/exampleText.ts
@@ -1,138 +1,112 @@
 export const exampleText = `
- <html>
-  <body>
-   <header>
-    <nav>
-      <table-of-contents></table-of-contents>
-    </nav>
-   </header>
-   <main>
-    <div>
-      <section>
-        <h1>H1 <strong>Example</strong></h1>
-        <h2>H2 Example</h2>
-        <h3>H3 Example</h3>
-        <h4>H4 Example</h4>
-        <h5>H5 Example</h5>
-        <h6>H6 Example</h6>
-        <article>
-          <p>
-            <span>
-              <s>Something</s>
-              <br/>
-              <i>sometimes</i>
-              <u>sometimes</u>
-              <strong>sometimes</strong>
-              <sub>sub</sub>
-              <sup>sup</sup>
-            </span>
-          </p>
-        </article>
-        <pre>
-        <article>
-          <p>
-            <span>
-              <s>Something</s>
-              <br/>
-              <i>sometimes</i>
-              <u>sometimes</u>
-              <strong>sometimes</strong>
-              <sub>sub</sub>
-              <sup>sup</sup>
-            </span>
-          </p>
-        </article>
-        <pre>
-      </section>   
-      <br/>
-      <br/>
-      <p>Something</p>
-      <strong>bold</strong>
-      <b>bold</b>
-      <em>italic</em>
-      <page-break/>
-      <p>
-        <a href='https://github.com/'>link</a>
-      </p>
-      <article>
-        <ul>
-          <li>List Item
-            <ul>
-              <li>list item</li>
-              <li>list item</li>
-              <li>list item</li>
-            </ul>
-          </li>
-          <li>List Item</li>
-          <li>List Item</li>
-        </ul>
-        <ol>
-          <li>List Item</li>
-          <li>List Item</li>
-          <li>List Item</li>
-        </ol>
-      </article>
-      <blockquote>
-        <p>Blockquote</p> and just text
-      </blockquote>
-      <figure class='image'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png'/>
-      </figure>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png'/>
-      <p>Paragraph</p>
-      
-      <figure class="table">
-        <table>
-          <colgroup>
-            <col style="width:14.29%;">
-            <col style="width:14.29px;">
-            <col style="width:14.29pt;">
-            <col style="width:14.29vw;">
-            <col style="width:14.29vh;">
-            <col style="width:14.29em;">
-            <col style="width:14.26rem;">
-          </colgroup>
-          <tbody>
-            <tr>
-              <td>das</td><td>sad</td><td>ss</td><td>d</td><td>&nbsp;</td><td>sda</td><td>dasdad</td>
-              </tr>
-            <tr>
-              <td>&nbsp;</td><td>asdas</td><td>ds</td><td>asd</td><td>sdsad</td><td>asd</td><td>sad</td>
-            </tr>
-            <tr>
-              <td>sd</td><td>sd</td><td>sd</td><td>sdsd</td><td>sd</td><td>sd</td><td>sd</td>
-            </tr>
-            <tr>
-              <td>d</td><td>sd</td><td>sd</td><td>sd</td><td>sd</td><td>sd</td><td>sd</td>
-            </tr>
-            <tr>
-              <td>sd</td><td>sd</td><td>sd</td><td>d</td><td>sd</td><td>sdsd</td><td>a</td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>
-            </tr>
-          </tbody>
-        </table>
-      </figure>
-    </div>
-   </main>
-   <aside>
-    <figure class='image'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png'/>
+  <h1 style="text-align: center;">Table of Contents</h1>
+  <br/>
+  <table-of-contents />
+
+  <page-break />
+
+  <h1 style="text-align: center;"><strong>Beautiful Places</strong></h1>
+  <br/>
+  <blockquote style="text-align: right;">
+    "The world is full of magical places, and Wulingyuan Scenic Area is one of them. Its stunning sandstone pillars and canyons will leave you in awe." 
+    <em>- Traveler</em>
+  </blockquote>
+  <br/>
+  
+  <h2 style="text-align: center;">The <i>Azores</i></h2>
+  <p>The <strong>Azores</strong> is a group of volcanic islands located in <u>Portugal</u>, known for their stunning landscapes, lush greenery, and beautiful blue lakes.</p>
+  <br/>
+  <h2 style="text-align: center;">Mount <strong>Kilimanjaro</strong></h2>
+  <p>Mount <i> Kilimanjaro</i> is the highest peak in <strong>Africa</strong>, situated in <strong>Tanzania</strong>. Its majestic snow-capped summit and diverse ecological zones attract adventurers and nature enthusiasts from around the world.</p>
+  <br/>
+  <h2 style="text-align: center;"><i>Wulingyuan Scenic Area</i></h2>
+  <p><strong>Wulingyuan Scenic Area</strong> is a <a href="https://whc.unesco.org/en/list/640">UNESCO World Heritage</a> located in <u>China</u>. It is renowned for its towering sandstone pillars, deep canyons, and breathtaking natural beauty, which served as an inspiration for the movie "Avatar."</p>
+   
+  <page-break />
+
+  <h2 style="text-align: center;"><strong>Additional Information</strong></h2>
+  <br/>
+  <h3>Top 3 Features of The Azores:</h3>
+  <ul>
+    <li>Stunning landscapes</li>
+    <li>Lush greenery</li>
+    <li>Beautiful blue lakes</li>
+  </ul>
+  <br/>
+  <h3>Top 3 Features of Mount Kilimanjaro:</h3>
+  <ol>
+    <li>Majestic snow-capped summit</li>
+    <li>Diverse ecological zones</li>
+    <li>Attracts adventurers and nature enthusiasts</li>
+  </ol>
+  <br/>
+  <h3>Top 3 Features of Wulingyuan Scenic Area:</h3>
+  <ul>
+    <li>Towering sandstone pillars</li>
+    <li>Deep canyons</li>
+    <li>Breathtaking natural beauty</li>
+  </ul>
+  
+  <br/>
+
+  <p>Experience the wonders of these beautiful places and create memories that will last a lifetime!</p>
+  <br/>
+  <table>
+    <colgroup>
+       <col style="width:60%;">
+       <col style="width:40%;">
+    </colgroup>
+    <thead>
+      <tr>
+        <th>Place</th>
+        <th>Country</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>The Azores</td>
+        <td>Portugal</td>
+      </tr>
+      <tr>
+        <td>Mount Kilimanjaro</td>
+        <td>Tanzania</td>
+      </tr>
+      <tr>
+        <td>Wulingyuan Scenic Area</td>
+        <td>China</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <page-break />
+
+  <h1 style="text-align: center;">Picture of most beautiful places</h1>
+  <br/>
+  <div>
+    <h2 style="text-align: center;">The Azores</h2>  
+    <figure>
+      <img style="width: 176px; float: left;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/A%C3%A7ores_2010-07-19_%285047589237%29.jpg/1200px-A%C3%A7ores_2010-07-19_%285047589237%29.jpg" alt="The Azores" />
     </figure>
-   </aside>
-   <footer>
-    <table>
-        <colgroup>
-          <col style="width:100%;">
-        </colgroup>
-        <tbody>
-          <tr>
-            <td>Example</td>
-          </tr>
-        </tbody>
-      </table>  
-   </footer>
- 
- 
+    <p>
+      The Azores, a group of volcanic islands situated in Portugal, captivate visitors with their landscapes, including rolling hills, dramatic coastlines, and vibrant crater lakes.
+    </p>
+  </div>
+  <br/>
+  <div>
+    <h2 style="text-align: center;">Mount Kilimanjaro</h2>
+    <figure>
+      <img style="width: 142px; float: right;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Mt._Kilimanjaro_12.2006.JPG/800px-Mt._Kilimanjaro_12.2006.JPG" alt="Mount Kilimanjaro" />
+    </figure>
+    <p>
+       Mount Kilimanjaro is the highest peak in Africa, situated in Tanzania. Its majestic snow-capped summit and diverse ecological zones attract adventurers ьfrom around the world.
+    </p>
+  </div>
+  <br/>
+  <div>
+    <h2 style="text-align: center;">Wulingyuan Scenic Area</h2>
+    <p>
+       Wulingyuan Scenic Area is a UNESCO World Heritage site located in China. It is renowned for its towering sandstone pillars, deep canyons, and breathtaking natural beauty.
+    </p>
+    <img style="width: 180px;" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Zhangjiajie-Hunan.jpg" alt="Wulingyuan Scenic Area" />
+  </div>
 `;
